d87c5388f6ef17c7b2c6506ed97aa5cf65217f6a
headline-design <aaron@headline.dev>
2021-12-11 11:10:26 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import './App.css';
+import { loadStdlib } from '@reach-sh/stdlib'
+import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
+import * as smartContract from './index.main.mjs';
+import Pipeline from '@pipeline-ui-2/pipeline'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const myAlgoWallet = Pipeline.init();
+
+console.log(Pipeline);
+
+var devnet = false;
+
+const reach = loadStdlib(devnet ? 'ALGO-devnet' : 'ALGO')
+
+//change Algo server IP to match your local reach devnet IP, or leave with "http://localhost" to test on local machine only.
+
+const myEnv = reach.providerEnvByName(devnet ? 'LocalHost' : 'MainNet');
+console.log(myEnv);
+var acct = {};
+
+if (devnet) {
+  myEnv.ALGO_INDEXER_SERVER = "http://localhost";
+  myEnv.ALGO_SERVER = "http://localhost";
+  reach.setProviderByEnv(myEnv);
+}
+
+reach.setWalletFallback(reach.walletFallback({
+  providerEnv: 'MainNet', MyAlgoConnect
+}));
+
+// accAlice = await reach.newTestAccount(startingBalance);
+//const accBob = await reach.newTestAccount(startingBalance);
+
+//const ctcAlice = accAlice.deploy(backend);
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      address: ""
+    }
+  }
+
+  deploy () {
+    acct.deploy(smartContract).then(data => {console.log(data)})
+  }
+
+  render() {
+    return (
+      <div className="reach" align="center">
+        <button onClick={() => Pipeline.connect(myAlgoWallet).then(
+          data => {
+            this.setState({address: data})
+            reach.getDefaultAccount(data).then(data2 => {
+              acct = data2;
+              console.log(data2);
+            })
+          })}>Connect</button>
+        <h3>{this.state.address}</h3>
+        <button onClick={this.deploy}>Deploy</button>
+      </div>
+    )
+  }
 }
 
 export default App;

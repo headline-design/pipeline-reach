fbad9e8510169ddc62ad2883300bbc8e4edae7c6
headline-design <aaron@headline.dev>
2021-12-12 18:57:18 -0600

    added teal voting smart contract

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import './App.css';
 import { loadStdlib } from '@reach-sh/stdlib'
 import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
 import { _Participants } from './index.main.mjs';
+import algosdk from 'algosdk'
+
+var sender = ""
+
+async function compileProgram(client, teal) {
+  let encoder = new TextEncoder();
+  let programBytes = encoder.encode(teal);
+  let compileResponse = await client.compile(programBytes).do();
+  return compileResponse;
+}
+
+const wallet = new MyAlgoConnect()
+
+var teal = ""
+var teal2 = ""
+
+async function getContract(file){
+  let data = await fetch(file)
+  data = await data.text()
+  return data
+}
+
+getContract('voting.txt').then(data => {teal = data; console.log(teal)})
+getContract('votingClear.txt').then(data => {teal2 = data; console.log(teal)})
 
 var contractName = "Morra Game"
 
@@ -29,14 +53,21 @@ reach.setWalletFallback(reach.walletFallback({
   providerEnv: 'MainNet', MyAlgoConnect
 }));
 
+
 const contracts = {
   "Morra Game": {
     contract: require('./index.main.mjs'),
     description: 'Game of two players guessing "fingers"',
-    interact: {...undefined,
+    interact: {
+      ..._Participants['Alice'],
       wager: reach.parseCurrency(0.001),
-      ...reach.hasConsoleLogger},
-      attach:{}
+      ...reach.hasConsoleLogger
+    },
+    attach: {
+      ..._Participants['Bob'],
+      acceptWager: (amt) => { console.log('Bob accepts: ' + amt)},
+      ...reach.hasConsoleLogger,
+    }
   }
 }
 
@@ -61,11 +92,14 @@ class App extends Component {
   }
 
   async attach() {
-
-    let ctcAlice = acct.contract(backend);
+    
+    let accAlice = acct
+    accAlice.networkAccount.addr = "C5E5W3BERJALL2ZH4YB3TAP7ZSJH2PJUPDHLGF74YE6DBMQ62AA47IXGNQ"
+    let ctcAlice = accAlice.contract(backend);
+    let ctcBob = acct.contract(backend, ctcAlice.getInfo());
 
     try {
-      await backend.Bob(ctcAlice, contracts[contractName].interact)
+      await backend.Bob(ctcBob, contracts[contractName].attach)
     }
     catch (error) { console.log(error) }
   }
@@ -76,11 +110,50 @@ class App extends Component {
       backend = contracts[event.target.value].contract;
       contractName = event.target.value;
       this.setState({ description: contracts[event.target.value].description });
-      try{this.setState({participants: Object.keys(_Participants).toString()})}catch(error){console.log(error)}
+      this.setState({participants: Object.keys(_Participants).toString()})
     }
     else {
       this.setState({ description: "" })
     }
+  }
+
+  async deployTeal(){
+    console.log("sender: " + sender)
+    const algodClient = new algosdk.Algodv2("",'https://api.testnet.algoexplorer.io', '');
+
+    let compiled = await compileProgram(algodClient, teal)
+    let compiledClear = await compileProgram(algodClient,teal2)
+
+    console.log(compiled)
+    const params = await algodClient.getTransactionParams().do();
+
+    console.log(params)
+
+    let registrationStart = algosdk.encodeUint64(params.firstRound)
+    let registrationEnd = algosdk.encodeUint64(params.firstRound + 1000)
+    let voteStart = algosdk.encodeUint64(params.firstRound + 2000)
+    let voteEnd = algosdk.encodeUint64(params.firstRound + 3000)
+
+    let appArgs = [registrationStart,registrationEnd,voteStart,voteEnd]
+    console.log(appArgs)
+    
+    const txn = algosdk.makeApplicationCreateTxnFromObject({
+        suggestedParams: {
+            ...params,
+        },
+        from: sender,
+        numLocalByteSlices: 1,
+        numGlobalByteSlices: 1,
+        numLocalInts: 0,
+        numGlobalInts: 6,
+        appArgs: appArgs,
+        approvalProgram: new Uint8Array(Buffer.from(compiled.result, "base64")),
+        clearProgram: new Uint8Array(Buffer.from(compiledClear.result, "base64")),
+        onComplete: 0,
+    });
+    
+    const signedTxn = await wallet.signTransaction(txn.toByte());
+    console.log(signedTxn)
   }
 
   render() {
@@ -96,12 +169,14 @@ class App extends Component {
           let address = data2.networkAccount.addr;
           acct = data2;
           this.setState({ address: address })
+          sender = address
           console.log(acct);
         })
         }>Connect</button>
         <h3>{this.state.address}</h3>
         <button onClick={this.deploy}>Deploy</button>
-        <button onClick={this.attach} disabled>Attach</button>
+        <button onClick={this.attach}>Attach</button><br></br>
+        <button onClick={this.deployTeal}>Deploy Teal Contract</button>
       </div>
     )
   }

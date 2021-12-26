2e3741b6b5c6dfde27ebb80f99a46dbb0000b565
headline-design <aaron@headline.dev>
2021-12-26 12:49:22 -0600

    updated build

--- build/static/js/react-syntax-highlighter_languages_refractor_ini.64956bfe.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppipeline-reach"]=this["webpackJsonppipeline-reach"]||[]).push([[60],{420:function(t,e,i){"use strict";function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.64956bfe.chunk.js.map

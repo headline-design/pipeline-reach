f28e5944e3c9354b4a0c76249a3b952c390f61c3
headline-design <aaron@headline-inc.com>
2021-12-30 01:45:46 +0000

    push-4

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_hsts.8d64e058.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppipeline-reach"]=this["webpackJsonppipeline-reach"]||[]).push([[55],{522:function(e,a,s){"use strict";function i(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=i,i.displayName="hsts",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.8d64e058.chunk.js.map
